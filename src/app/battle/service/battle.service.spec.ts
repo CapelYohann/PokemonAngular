4739e1043e60d1037d6c355ca75f5e4e03942b11
Kyoya <mouadh95@live.fr>
2020-02-21 09:44:41 +0100

    test battle first

--- src/app/battle/service/battle.service.spec.ts
+++ src/app/battle/service/battle.service.spec.ts
@@ -2,15 +2,56 @@ import { TestBed } from '@angular/core/testing';
 
 import { BattleService } from './battle.service';
 
+
+class MockLoggerService extends LoggerService {
+};
+
 describe('BattleService', () => {
   let service: BattleService;
+  let mockService: MockLoggerService;
 
-  beforeEach(() => {
+  /*beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(BattleService);
+  });*/
+
+
+  beforeEach(() => { (2)
+    mockService = new MockLoggerService();
+    service = new BattleService(mockService);
+  });
+
+  afterEach(() => {
+    mockService = null;
+    service = null;
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should initialize a round', () => {
+  	service.init();
+    expect(service.get().length).toEqual(1);
+  });
+
+  it('should add several lines to log', () => {
+  	service.add("one line", "pikachu");
+  	service.add("two line", "pikachu");
+  	service.add("three line", "pikachu");
+    expect(service.get().length).toEqual(3);
+  });
+
+  it('added line should contain one pokemon\'s line', () => {
+  	service.add("one line", "pikachu");
+  	let added: Line[];
+  	added = service.get();
+    expect(added[0].pokemon).toBe("pikachu");
+  });
+
+  it('should clear log', () => {
+  	service.add("one line", "pikachu");
+  	service.clear();
+    expect(service.get().length).toEqual(0);
   });
 });
